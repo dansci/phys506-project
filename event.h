6931a95ed2557a757f08750c8797e614c1e4a7d6
Daniel Scislowski <dansci@uw.edu>
2011-05-24 13:41:55 -0700

    wrote inefficient storage for the position distribution, and functions to fill the data part of them.  let's see if they compile...

--- event.h
+++ event.h
@@ -1,11 +1,27 @@
 const double scint_time = 5e-9; /* 5ns? */
 const double light_speed = 3e8;
 
+struct pmtmap {
+     int N; /* number of PMTS */
+     int ntheta, nphi; /* division of theta, phi directions */
+     struct pmt *pmt;
+};
+
+struct pmt {
+     double x[3]; /* position in cartesian */
+     double p[3]; /* and polar coords */
+}
+
 struct event {
+     /* timing fitter info */
      int N; /* number of photon hits */
      double *spawn_pos;
      double spawn_time;
      struct hit *hits;
+
+     /* pos distn fitter info */
+     double *pmt_hits; /* number of hits per PMT */
+     double *e_pmt_hits; /* expected number of hits per PMT */
 };
 
 struct hit {
@@ -28,8 +44,12 @@ void sort_event(struct event *e)
 
 static double dir[3]; /* avoids having to malloc every time */
 		 
-/* make_event/2: fill event structure *E with N hits originating from
- * same (random) position*/
+/*
+ * make_event/2: fill event structure *E with N hits originating from
+ * same (random) position.
+ *
+ * NB, this only fills the parts relevant for the timing fitter so far
+ */
 void make_event(struct event *e, int N)
 {
      e->N = N;
@@ -53,4 +73,50 @@ void make_event(struct event *e, int N)
 	       e->spawn_time + sample_scint_time(scint_time) +
 	       vec_distance(e->spawn_pos, e->hits[i].hit_pos, 3)/light_speed;
      }
+}
+
+void fill_pmt_info(struct event *e, struct pmtmap *p)
+{
+     int i;
+     /* clear the PMT hits initially */
+     for (i=0; i<p->N; i++)
+	  e->pmt_hits[i] = 0;
+
+     /* loop over photons, incrementing the relevant PMT number */
+     for (i=0; i<e->N; i++)
+	  e->pmt_hits[get_pmt_number(p, e->hits[i].hit_pos)]++;
+}
+
+int get_pmt_number(struct pmtmap *p, double *pos)
+{ 
+     int ret = 0;
+     /* we're dividing the detector by angle, so position in angle is
+      * much more useful to us.  Maybe we'll put that information
+      * directly in a struct one day */
+
+     /* FIXME: inefficient; not using all the info available to us */
+     double r,theta, phi;
+     r = sqrt(pos[0]*pos[0] + pos[1]*pos[1] + pos[2]*pos[2]);
+     theta = acos(pos[2]/r);
+     phi = atan(pos[1]/pos[0]);
+     
+     double dtheta = M_PI / (2*p->ntheta); /* the regions covered by */
+     double dphi = 2*M_PI / (2*p->nphi);   /* the pmt from its center*/
+
+     int i;
+     for (i=0; ; i++)
+	  if (theta > p->pmt[i].p[1] + dtheta &&
+	      theta < p->pmt[i+1].p[1] - dtheta) {
+	       ret += i*p->nphi;
+	       break;
+	  }
+
+     for (i=0; ; i++)
+	  if (phi > p->pmt[i].p[2] + dphi &&
+	      phi < p->pmt[i+1].p[2] - dphi) {
+	       ret += i;
+	       break;
+	  }
+
+     return ret;
 }
