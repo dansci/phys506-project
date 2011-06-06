791a06cf74442901e3f8f1d7a0ab818dc17d555e
Daniel Scislowski <dansci@u.washington.edu>
2011-06-06 11:29:36 -0700

    edited the data collection so that files will be named correctly next time

--- tests/combined_fit/data_collection.sh
+++ tests/combined_fit/data_collection.sh
@@ -14,7 +14,7 @@ while [ $NHITS -le $NHITSMAX ]; do
     echo "on nhits=$NHITS"
     while [ $GSL_RNG_SEED -le $NTRIALS ]; do
 	export GSL_RNG_SEED
-	$PROG -N $NHITS -t $TOL >> times_N${NHITS}_t$TOL.txt
+	$PROG -N $NHITS -t $TOL >> comb_N${NHITS}_t$TOL.txt
 	GSL_RNG_SEED=$(( GSL_RNG_SEED + 1 ))
     done
     NHITS=$(( NHITS + NHITSSTRIDE ))
